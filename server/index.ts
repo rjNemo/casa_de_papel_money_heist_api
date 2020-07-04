20662860e88ac1d51c7010204e732c2fb653a6dd
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-04 14:39:24 +0200

    enable handling routes

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,12 @@ import express, { Application } from "express";
 import compression from "compression";
 import helmet from "helmet";
 import { connect } from "mongoose";
-
+// log
 import { logDbSuccess, logServerStart } from "./helpers/";
+// routes
+import * as ROUTES from "./constants/routes";
+import characters from "./routes/api/characters";
+import episodes from "./routes/api/episodes";
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -12,20 +16,21 @@ if (process.env.NODE_ENV !== "production") {
 const app: Application = express();
 
 const PORT = process.env.PORT ?? "5000";
-
 // connect to db
 const DB_URI = process.env.MONGO_URI ?? "localdb";
 
 connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(logDbSuccess)
-  .catch((err) => console.error(err)); //TODO: handle connection errors
+  .catch((err) => console.error(err)); // TODO: handle connection errors
 
 // Middlewares
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello"));
+// routes
+app.use(ROUTES.CHARACTERS, characters);
+app.use(ROUTES.EPISODES, episodes);
 
 // start server
 app.listen(PORT, logServerStart(PORT));
