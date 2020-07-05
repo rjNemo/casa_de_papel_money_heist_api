decf718a7878798c1063e53744ac01a44f87df30
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-05 15:27:47 +0200

    feat(characters): add post character route

--- server/routes/api/characters.ts
+++ server/routes/api/characters.ts
@@ -20,4 +20,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+/** Post a character. TODO: accessible only after admin authentication */
+router.post("/", async (req, res) => {
+  const character = new Character({
+    name: req.body.name,
+    nickname: req.body.nickname,
+    status: req.body.status,
+    gender: req.body.gender,
+    image: req.body.image,
+    occupation: req.body.occupation,
+    appearances: req.body.appearances,
+    portrayed: req.body.portrayed,
+  });
+
+  try {
+    const resp = await character.save();
+    res.status(201).json(resp);
+  } catch (error) {
+    res.status(500).json({ error: "Invalid Request" });
+  }
+});
+
 export default router;
