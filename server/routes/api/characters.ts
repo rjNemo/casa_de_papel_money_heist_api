4cac86a6d64557d6a6c38aeee2fd0b8b3226788c
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-05 15:05:03 +0200

    feat(characters): add find character by id

--- server/routes/api/characters.ts
+++ server/routes/api/characters.ts
@@ -4,9 +4,20 @@ import Character from "../../models/Character";
 
 const router = Router();
 
+/** Get all characters */
 router.get("/", async (_, res) => {
   const chars = await Character.find();
   res.json(chars);
+});
+
+/** Get a character by ID */
+router.get("/:id", async (req, res) => {
+  try {
+    const chars = await Character.findById(req.params.id);
+    res.json(chars);
+  } catch (error) {
+    res.status(404).json({ error: "Character not found." });
+  }
 });
 
 export default router;
