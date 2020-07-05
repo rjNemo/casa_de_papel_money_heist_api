ccb801e22d8db833e0d377c1a0b66d680ec877c6
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-07-05 15:36:09 +0200

    feat(episode): add post route

--- server/routes/api/episodes.ts
+++ server/routes/api/episodes.ts
@@ -9,4 +9,22 @@ router.get("/", async (_, res) => {
   res.json(chars);
 });
 
+/** Post a episode. TODO: accessible only after admin authentication */
+router.post("/", async (req, res) => {
+  const episode = new Episode({
+    title: req.body.title,
+    season: req.body.season,
+    episode: req.body.episode,
+    air_date: req.body.air_date,
+    characters: req.body.characters,
+  });
+
+  try {
+    const resp = await episode.save();
+    res.status(201).json(resp);
+  } catch (error) {
+    res.status(500).json({ error: "Invalid Request" });
+  }
+});
+
 export default router;
